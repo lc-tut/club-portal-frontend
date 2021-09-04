50493b66288b6751bf8ad7f366ee7b536c3d8a82
caffeine <kouta8145963@gmail.com>
2021-09-04 19:03:09 +0900

    :bug: Fix: Header height

--- src/components/global/Header.tsx
+++ src/components/global/Header.tsx
@@ -1,10 +1,13 @@
 import { BellIcon, HamburgerIcon } from "@chakra-ui/icons"
 import { Avatar, Box, Center, Flex, Heading, Spacer } from "@chakra-ui/react"
+import { stat } from "fs";
 import { PortalLogo } from "../common/Logo"
 
 export const Header: React.VFC<{}> = () => {
+  const headerHeight:number = 52;
+
   return (
-    <Flex px="5" h="5%" w="100%" backgroundColor="green.200">
+    <Flex px="15" h={headerHeight + "px"} w="100%" backgroundColor="green.200">
       <Center>
         <HamburgerIcon boxSize="2em"/>
       </Center>
