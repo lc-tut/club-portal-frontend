4122676261281cbe102ed71606f40d6fd0447c3b
caffeine <kouta8145963@gmail.com>
2021-09-05 17:29:20 +0900

    :art: Clean: code formatting

--- src/components/common/DefaultUserIcon.tsx
+++ src/components/common/DefaultUserIcon.tsx
@@ -3,6 +3,8 @@ import type { IconProps } from "@chakra-ui/react"
 import type { PropsWithChildren } from "react"
 import { ReactComponent as IconImage } from "../../static/DefaultUserIcon.svg"
 
-export const DefaultUserIcon: React.VFC<PropsWithChildren<IconProps>> = (props) => {
+export const DefaultUserIcon: React.VFC<PropsWithChildren<IconProps>> = (
+  props
+) => {
   return <Icon as={IconImage} {...props} />
 }
