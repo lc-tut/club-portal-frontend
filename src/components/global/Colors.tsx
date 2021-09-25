330f9e9b715e097ba3c009ddcf2e7a6b152b6453
caffeine <kouta8145963@gmail.com>
2021-09-25 16:23:58 +0900

    :white_check_mark: update: custom color theme

--- src/components/global/Colors.tsx
+++ src/components/global/Colors.tsx
@@ -5,8 +5,31 @@ const theme = extendTheme({
         text: {
             "main": "#626262",
             "sub": "#A0A0A0",
-            "test": "#ff0000"
-        }
+            card: {
+                "main": "#7BB194",
+                "sub": "#8E958C",
+            },
+            modal: {
+                "main": "#19342B",
+                "sub": "#818684",
+            },
+            title: {
+                "sub": "#888888"
+            }
+        },
+        button: {
+            "yellow": "#EDBD17",
+            "orange": "#ED8131",
+        },
+        form: {
+            "frame": "#C1C1C1",
+            "background": "#F5F5F5",
+        },
+        background: {
+            "main": "#FFFFFC",
+            "cards": "#F7F1E5"
+        },
+        separator: "#DEDEDE",
     }
 })
 
