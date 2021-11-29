2544e75d73a6ddea729aaeb1e35dfadf660654d5
caffeine <kouta8145963@gmail.com>
2021-11-29 11:26:16 +0900

    change: Chakra-UI Icon -> Bootstrap Icon

--- src/components/global/Header.tsx
+++ src/components/global/Header.tsx
@@ -1,5 +1,5 @@
-import { BellIcon, HamburgerIcon } from "@chakra-ui/icons"
-import { Center, Flex, Spacer } from "@chakra-ui/react"
+import { BellFill, List } from "react-bootstrap-icons"
+import { Center, Flex, Spacer, Icon } from "@chakra-ui/react"
 import { DefaultUserIcon } from "../common/DefaultUserIcon"
 
 export const Header: React.VFC<{}> = () => {
@@ -8,11 +8,11 @@ export const Header: React.VFC<{}> = () => {
   return (
     <Flex px="15" h={`${headerHeight}px`} w="100%" backgroundColor="green.200">
       <Center>
-        <HamburgerIcon boxSize="2em" />
+        <Icon as={List} boxSize="2em" color="text.title.sub" />
       </Center>
       <Spacer />
-      <Center>
-        <BellIcon boxSize="2em" mx="3" />
+      <Center mr="15px">
+        <Icon as={BellFill} boxSize="2em" color="text.title.sub" />
       </Center>
       <Center>
         <DefaultUserIcon boxSize="2em" />
