ec9428663a402a7da67058aadeadf800a48305f9
caffeine <kouta8145963@gmail.com>
2021-12-02 04:07:11 +0900

    add: new files

---
+++ src/components/global/TitleArea.tsx
@@ -0,0 +1,6 @@
+import { Box } from "@chakra-ui/react"
+import { PropsWithChildren } from "react"
+
+export const TitleArea: React.VFC<PropsWithChildren<{}>> = (props) => {
+  return <Box>title</Box>
+}
---
+++ src/pages/clubs.tsx
@@ -0,0 +1,9 @@
+import { Box } from "@chakra-ui/react"
+
+const AnimatedClubs: React.VFC<{}> = () => {
+  return <Box>clubs</Box>
+}
+
+export const Clubs: React.VFC<{}> = () => {
+  return <AnimatedClubs />
+}
