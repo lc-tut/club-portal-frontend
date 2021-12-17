6a78e3d32230776b5cb1d1e19647c57a9967f9e6
kash / Hytus <kash@hytus.net>
2021-12-18 05:03:42 +0900

    :label: エラーの暫定処理
    Signed-off-by: kash / Hytus <kash@hytus.net>

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -4,5 +4,7 @@ import type { PropsWithChildren } from "react"
 import Logo from "../../static/Logo.svg"
 
 export const PortalLogo: React.VFC<PropsWithChildren<IconProps>> = (props) => {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  //@ts-ignore
   return <Icon as={Logo} {...props} />
 }
