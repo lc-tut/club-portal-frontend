6ddb4148c36d08485b98da5f671b80b616f0fda9
kash / Hytus <kash@hytus.net>
2021-12-17 16:00:12 +0900

    lint
    Signed-off-by: kash / Hytus <kash@hytus.net>

--- src/components/common/Button/MenuButton.stories.tsx
+++ src/components/common/Button/MenuButton.stories.tsx
@@ -1,16 +1,18 @@
-import { MenuButton } from ".";
+import { MenuButton } from "."
 
-import type { ComponentStory, ComponentMeta } from '@storybook/react'
+import type { ComponentStory, ComponentMeta } from "@storybook/react"
 
 export default {
   title: "MenuButton",
   component: MenuButton,
 } as ComponentMeta<typeof MenuButton>
 
-const Template: ComponentStory<typeof MenuButton> = (args) => <MenuButton {...args} />
+const Template: ComponentStory<typeof MenuButton> = (args) => (
+  <MenuButton {...args} />
+)
 
 export const MenuButtonStory = Template.bind({})
 
 MenuButtonStory.args = {
-  children: "foo"
+  children: "foo",
 }
