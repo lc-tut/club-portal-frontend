0c0c908ed3ffb1bc012b1052658668bff80b07d2
caffeine <kouta8145963@gmail.com>
2022-01-30 05:00:09 +0900

    wip: description page

--- src/pages/clubDescription.tsx
+++ src/pages/clubDescription.tsx
@@ -1,10 +1,26 @@
-import { Box } from "@chakra-ui/react"
-
+import { HStack, Icon, VStack, Flex } from "@chakra-ui/react"
+import { TitleArea } from "../components/global/TitleArea"
+import { ClubTypeBadge } from "../components/common/ClubTypeBadge"
+import { BsClock, BsStar } from "react-icons/bs"
+import { PortalButton } from "../components/common/Button"
 
 const AnimatedClubDescription: React.VFC<{}> = () => {
-  return <Box>
-    this is club description page
-  </Box>
+  return <VStack flex="1">
+    <TitleArea subtitle="サークルの簡単な説明">
+      サークル名
+    </TitleArea>
+    <HStack>
+      <ClubTypeBadge content="hachioji" badgetype="page" />
+      <ClubTypeBadge content="culture" badgetype="page" />
+      <Flex color="text.sub" alignItems="center">
+        <Icon as={BsClock} mr="5px" />
+        最終更新: 2022 1/30
+      </Flex>
+      <PortalButton pbcolor="yellow" leftIcon={<BsStar/>}>
+        お気に入り
+      </PortalButton>
+    </HStack>
+  </VStack>
 }
 
 export const ClubDescription: React.VFC<{}> = () => {
