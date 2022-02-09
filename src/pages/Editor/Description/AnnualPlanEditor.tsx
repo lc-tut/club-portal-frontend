29b582699ec95d5fba031a726f0e3cc5fcc2ca0b
caffeine <kouta8145963@gmail.com>
2022-02-10 06:54:03 +0900

    add: AnnualPlanEditor

---
+++ src/pages/Editor/Description/AnnualPlanEditor.tsx
@@ -0,0 +1,78 @@
+import { VStack, Text, Grid, Stack, Textarea, GridItem } from "@chakra-ui/react"
+import { Dispatch, SetStateAction, useState } from "react"
+import { PortalButton } from "../../../components/common/Button"
+import { EditorBase } from "../../../components/common/Editor/EditorBase"
+import { TitleArea } from "../../../components/global/TitleArea"
+import { PADDING_BEFORE_FOOTER } from "../../../static/consts"
+
+const months = [
+  4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3
+]
+
+type MonthInputAreaProps = {
+  month: number
+  inputData: string[]
+  setInputData: Dispatch<SetStateAction<string[]>>
+}
+
+const MonthInputArea: React.VFC<MonthInputAreaProps> = (props) => {
+  return (
+    <Stack>
+      <Text pl="0.2rem">
+        {props.month.toString() + "月"}
+      </Text>
+      <Textarea
+        w="20rem"
+        h="4rem"
+        backgroundColor="#fff"
+        value={props.inputData[props.month]}
+        onChange={(e)=>{
+          const newInputData = [...props.inputData]
+          newInputData[props.month] = e.target.value
+          props.setInputData(newInputData)
+        }}
+      />
+    </Stack>
+  )
+}
+
+export const AnualPlanEditor: React.VFC<{}> = () => {
+  // ここでデータを取得するが、要素数が12なるようにする
+  const dummy = [
+    "1月の予定", "", "",
+    "", "5月の予定", "",
+    "7月の予定", "", "",
+    "", "11月の予定", ""
+  ]
+  const [ inputData, setInputData ] = useState(dummy)
+
+  return (
+    <VStack flex="1" pb={PADDING_BEFORE_FOOTER}>
+      <TitleArea>
+        年間予定の編集
+      </TitleArea>
+      <EditorBase>
+        <Grid
+          templateColumns={{base: "repeat(1, 1fr)", md: "repeat(2, 1fr)"}}
+          columnGap="2rem"
+          rowGap="2rem"
+        >
+          {
+            months.map((item) => {
+              return (
+                <GridItem key={item}>
+                  <MonthInputArea
+                    month={item}
+                    inputData={inputData}
+                    setInputData={setInputData}
+                  />
+                </GridItem>
+              )
+            })
+          }
+        </Grid>
+        <PortalButton> 保存 </PortalButton>
+      </EditorBase>
+    </VStack>
+  )
+}
--- src/pages/Editor/Description/DescriptionEditorRouter.tsx
+++ src/pages/Editor/Description/DescriptionEditorRouter.tsx
@@ -1,12 +1,13 @@
 import { useParams } from "react-router-dom"
 import { NotFound } from "../../notfound"
+import { AnualPlanEditor } from "./AnnualPlanEditor"
 import { ClubDescriptionEditor } from "./ClubDescriptionEditor"
 import { DetailInformationEditor } from "./DetailInformation/DetailInformationEditor"
 
 const pageMap: { [key in string]: JSX.Element } = {
   "club-description": <ClubDescriptionEditor />,
   "detail-information": <DetailInformationEditor />,
-  "annual-plan": <></>,
+  "annual-plan": <AnualPlanEditor />,
   links: <></>,
   video: <></>,
   pictures: <></>,
