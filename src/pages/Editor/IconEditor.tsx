8b7b259e401d5d392def694415ec85bdb32d76f9
caffeine <kouta8145963@gmail.com>
2022-02-12 05:38:37 +0900

    wip: IconEditor

--- src/pages/Editor/IconEditor.tsx
+++ src/pages/Editor/IconEditor.tsx
@@ -1,5 +1,31 @@
-import { Text } from "@chakra-ui/react"
+import { Input, Text, VStack } from "@chakra-ui/react"
+import { createRef, useState } from "react"
+import { PortalButton } from "../../components/common/Button"
+import { EditorBase } from "../../components/common/Editor/EditorBase"
+import { TitleArea } from "../../components/global/Header/TitleArea"
+import { PADDING_BEFORE_FOOTER } from "../../static/consts"
 
 export const IconEditor: React.VFC<{}> = () => {
-  return <Text>this is icon editor</Text>
+  const [ icon, setIcon ] = useState<string>("")
+  const [ inputData, setInputData ] = useState<string>()
+  const inputRef = createRef<HTMLInputElement>()
+
+  return (
+    <VStack flex="1" pb={PADDING_BEFORE_FOOTER}>
+      <TitleArea>
+        サークルアイコンの変更
+      </TitleArea>
+      <Input
+        type="file"
+        accept="image/png, image/jpeg"
+        display="none"
+        ref={inputRef}
+      />
+      <EditorBase>
+        <PortalButton onClick={() => inputRef.current?.click()}>
+          画像をアップロード
+        </PortalButton>
+      </EditorBase>
+    </VStack>
+  )
 }
