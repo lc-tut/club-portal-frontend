60bcd1b4694de54f218c87c4b1065066ccfd5d47
caffeine <kouta8145963@gmail.com>
2022-03-12 14:23:17 +0900

    update: 404 page

--- src/pages/notfound.tsx
+++ src/pages/notfound.tsx
@@ -1,11 +1,21 @@
-import { Link } from "react-router-dom"
+import { Heading, Text, VStack } from "@chakra-ui/react"
 import { motion } from "framer-motion"
+import { TitleArea } from "../components/global/Header/TitleArea"
 
 export const NotFound: React.VFC<{}> = () => {
   return (
-    <motion.div>
-      <h1>Not Found</h1>
-      <Link to="/">Back</Link>
-    </motion.div>
+    <>
+      <motion.div>
+        <VStack w="100vw">
+          <TitleArea>
+            404 Not Found
+          </TitleArea>
+          <Heading color="green.900" pt="5rem">
+            お探しのページは見つかりませんでした
+          </Heading>
+          <Text color="text.main">URLにタイプミスが無いかご確認下さい。</Text>
+        </VStack>
+      </motion.div>
+    </>
   )
 }
