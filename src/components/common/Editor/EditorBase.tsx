e573217010b1dfcc49cd71b9d71d9b5f51211c75
CaffeineFree <48763656+caffeine0coffee@users.noreply.github.com>
2022-03-17 19:42:36 +0900

    編集者ページに戻るボタンを追加 (#254)
    * add: EditorBackButton
    
    * fix: format

--- src/components/common/Editor/EditorBase.tsx
+++ src/components/common/Editor/EditorBase.tsx
@@ -1,17 +1,27 @@
-import { Flex, VStack } from "@chakra-ui/react"
+import { Flex, Stack, VStack, Wrap } from "@chakra-ui/react"
 import type { PropsWithChildren } from "react"
+import { EditorBackButton } from "./EditorBackButton"
 
-export const EditorBase: React.VFC<PropsWithChildren<{}>> = (props) => {
+type EditorBaseProps = {
+  noBackButton?: boolean
+}
+
+export const EditorBase: React.VFC<PropsWithChildren<EditorBaseProps>> = (
+  props
+) => {
   return (
-    <Flex pt="3rem">
-      <VStack
-        backgroundColor="form.background"
-        py="3rem"
-        px="4rem"
-        spacing="4rem"
-      >
-        {props.children}
-      </VStack>
+    <Flex pt="1rem">
+      <Stack>
+        <Wrap h="2rem">{!props.noBackButton && <EditorBackButton />}</Wrap>
+        <VStack
+          backgroundColor="form.background"
+          py="3rem"
+          px="4rem"
+          spacing="4rem"
+        >
+          {props.children}
+        </VStack>
+      </Stack>
     </Flex>
   )
 }
