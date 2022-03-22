80dfef5d233a2764966757b7c5d9acac685ea8fc
caffeine <kouta8145963@gmail.com>
2022-03-23 02:34:59 +0900

    update: annual plan order

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -17,7 +17,7 @@ export const ACTIVITY: ReadonlyArray<BadgeActivity> = [
   "committee",
 ]
 export const MONTHS: ReadonlyArray<number> = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+  4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3,
 ]
 
 export const BUILDING_ID_MAP: { [key in number]: string } = {
