ab707517b6d235490cf005f1474e88dcd9561eca
kash / Hytus <kash@hytus.net>
2022-04-05 17:16:19 +0900

    :art: format
    Signed-off-by: kash / Hytus <kash@hytus.net>

--- src/components/common/Clubs/ClubSortOptionSelect.tsx
+++ src/components/common/Clubs/ClubSortOptionSelect.tsx
@@ -1,6 +1,11 @@
 import { Select } from "@chakra-ui/react"
 import type { Dispatch } from "react"
-import type { FilterActionType, FilterStateType, SET_NAME_ASC_ACTION, SET_NAME_DESC_ACTION } from "../../../types/reducer"
+import type {
+  FilterActionType,
+  FilterStateType,
+  SET_NAME_ASC_ACTION,
+  SET_NAME_DESC_ACTION,
+} from "../../../types/reducer"
 
 type ClubSortOptionSelectProps = {
   isAscending: FilterStateType["isAscending"]
@@ -19,7 +24,11 @@ export const ClubSortOptionSelect: React.VFC<ClubSortOptionSelectProps> = (
       iconColor="text.card.main"
       value={props.isAscending ? "SET_NAME_ASC" : "SET_NAME_DESC"}
       onChange={(e) => {
-        props.dispatchIsAscending({ type: e.target.value as unknown as SET_NAME_ASC_ACTION | SET_NAME_DESC_ACTION })
+        props.dispatchIsAscending({
+          type: e.target.value as unknown as
+            | SET_NAME_ASC_ACTION
+            | SET_NAME_DESC_ACTION,
+        })
       }}
     >
       <option value="SET_NAME_ASC">名前 - 昇順</option>
