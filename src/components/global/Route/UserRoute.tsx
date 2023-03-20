aa1c1aab05c9d837ebaddb8c56f43c1e6c37d118
kash / Hytus <kash@hytus.net>
2023-03-20 21:07:35 +0900

    :zap: improved loading UI
    Signed-off-by: kash / Hytus <kash@hytus.net>

--- src/components/global/Route/UserRoute.tsx
+++ src/components/global/Route/UserRoute.tsx
@@ -1,26 +1,23 @@
 import axios from "axios"
 import { Outlet } from "react-router-dom"
+import { useLoadingStateContext } from "../../../contexts/loading"
 import { useAPI } from "../../../hooks/useAPI"
 import { NotFound } from "../../../pages"
 import { ErrorPage } from "../../../pages/error"
 import type { UserInfo } from "../../../types/api"
-import { Loading } from "../LoadingPage"
 
 // TODO: return Unauthorized instead of NotFound
 export const UserRouteElement: React.FC<{}> = () => {
-  const { data, isLoading, isError } = useAPI<UserInfo>("/api/v1/users")
+  const { data } = useAPI<UserInfo>("/api/v1/users")
+  const { isLoading, error } = useLoadingStateContext()
 
-  if (isLoading) {
-    return <Loading fullScreen />
-  }
-
-  if (isError) {
-    if (axios.isAxiosError(isError) && isError.response?.status === 401) {
+  if (error) {
+    if (axios.isAxiosError(error) && error.response?.status === 401) {
       return <NotFound />
     } else {
       return <ErrorPage />
     }
   }
 
-  return <Outlet context={data} />
+  return <>{!isLoading && data && <Outlet context={data} />}</>
 }
--- src/hooks/useSession.ts
+++ src/hooks/useSession.ts
@@ -3,26 +3,22 @@ import { useEffect } from "react"
 import useSWR from "swr"
 import { useSetLoadingStateContext } from "../contexts/loading"
 import type { Session } from "../types/api"
-import { axiosFetcher } from "../utils/axios"
 
 export const useSession = () => {
-  const { data, error } = useSWR<Session, Error | AxiosError>(
-    "/api/auth",
-    axiosFetcher
+  const { setIsLoading } = useSetLoadingStateContext()
+  const { data, error, isLoading } = useSWR<Session, Error | AxiosError>(
+    "/api/auth"
   )
-  const setLoadingState = useSetLoadingStateContext()
-  const isLoading = !error && data === undefined
 
   useEffect(() => {
-    if (!isLoading || error) {
-      setLoadingState.setIsLoading(isLoading)
-      setLoadingState.setIsError(error)
+    if (isLoading) {
+      setIsLoading(true)
     }
   })
 
   return {
     session: data as Session,
     isLoading: isLoading,
-    isError: error,
+    error: error,
   }
 }
