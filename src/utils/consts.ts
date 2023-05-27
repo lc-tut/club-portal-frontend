679c79038c3491a8ca25b4721bcba432dc4c0882
kash / Hytus <kash@hytus.net>
2023-05-28 00:18:11 +0900

    :ambulance: fix type bug of swiper * add self prepared swiper.d.ts :wrench: update tsconfig.json * moduleResolution is now bundler :art: format
    Signed-off-by: kash / Hytus <kash@hytus.net>

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -20,7 +20,7 @@ export const VALID_SNS_LIST: ReadonlyArray<LinkType> = [
   "Instagram",
   "Discord",
   "LINE",
-  "YouTube"
+  "YouTube",
 ]
 export const CAMPUS: ReadonlyArray<BadgeCampus> = ["hachioji", "kamata"]
 export const ACTIVITY: ReadonlyArray<BadgeActivity> = [
